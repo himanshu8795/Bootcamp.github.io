f903521385ced34157ddae44dca2a4351ca562f7
himanshu8795 <61570488+himanshu8795@users.noreply.github.com>
2020-07-28 22:33:37 +0530

    Add files via upload
    initial commit

---
+++ dogname.js
@@ -0,0 +1,5 @@
+let name = "Gator";
+let age = 1;
+age = age + 1;
+
+console.log('My dog ' + name + ' is turning ' + age + '.');
---
+++ index.js
@@ -0,0 +1,10 @@
+let firstName = "Dylan";
+firstName = 'Zack';
+
+let lastName = "Israel";
+let fullName = lastName + ", " + firstName;
+let age = 32;
+let isEmployed = true;
+isEmployed = false;
+
+console.log(fullName);
