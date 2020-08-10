f987ef54281d2c37ba4d05d052e449ad53ebddba
himanshu8795 <61570488+himanshu8795@users.noreply.github.com>
2020-08-10 20:54:25 +0530

    Add files via upload
    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
-import Express from "express";
+import Express from 'express';
 
-const app =Express();
-const port =3000;
+var app =Express();
+app.set('view engine', 'ejs');
+const port =4040;
 
-app.get("/" , (req,res) => {
-  res.send("hello world");
+app.get('/' ,(req,res) => {
+  res.send('i am new here')
 })
+
+app.get('/profile/:name', (req, res) => {
+  res.render('profile.ejs', {person : req.params.name});
+})
+
+
 
 app.listen(port, ()=> console.log("listening on port" + port))
